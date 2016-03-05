1e8cb464c9f34c67a05ff9e2f1c3004089e035fd
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-03-04 21:01:22 -0300

    erasing files

--- DOM/exemplo2/exemplo2.js
+++
@@ -1,8 +0,0 @@
-// criar um formulario de IMC
-
-function imc() {
-  var peso = document.getElementById('peso').value;
-  var altura = document.getElementById('altura').value;
-  var imc = peso / (altura*altura);
-  document.getElementById('result').innerHTML = imc.toFixed(2);
-}
