d379d1090938923d183eeeb6cc739f3c07186830
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-03-11 10:08:37 -0300

    preparing app for students

--- angular-phone/js/controllers.js
+++ angular-phone/js/controllers.js
@@ -1,23 +1,6 @@
 phoneList
 
 .controller('phoneListCtrl', [
-  '$scope',
 
-  function ($scope) {
-    $scope.app = "Phone List";
-
-    $scope.contactList = [
-      {
-        name: 'Hugo',
-        number: '9999-9999'
-      },
-      {
-        name: 'Hugo',
-        number: '9999-9999'
-      },
-      {
-        name: 'Hugo',
-        number: '9999-9999'
-      }
-    ];
-}])
+  function () {}
+]);
