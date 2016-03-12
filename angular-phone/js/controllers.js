4e174f0b4aa5414dae9586f1b8afcd9ae9d4bad8
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-03-12 17:09:04 -0300

    add exercice 15

--- angular-phone/js/controllers.js
+++ angular-phone/js/controllers.js
@@ -2,26 +2,70 @@ phoneList
 
 .controller('phoneListCtrl', [
   '$scope',
+  '$http',
 
-  function ($scope) {
+  function ($scope, $http) {
+    var getContacts = function () {
+      var url = "https://vast-mesa-46305.herokuapp.com/api/v1/contacts";
+      var params = {
+        nick: 'superman'
+      };
+      $http.get(url, { params: params })
+        .then(function (response) {
+          console.log('terminei de preencher a lista');
+          $scope.contactList = response.data.contacts;
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    }
+
+    var saveContact = function (contact) {
+      var url = "https://vast-mesa-46305.herokuapp.com/api/v1/contacts";
+
+      var data = {
+        contact: {
+          name: contact.name,
+          number: contact.number,
+          carrier_id: 1
+        },
+        nick: 'superman'
+      }
+
+      $http.post(url, data)
+        .then(function (response) {
+          console.log(response);
+          getContacts();
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    }
+
+    var removeContact = function (id) {
+      var url = "https://vast-mesa-46305.herokuapp.com/api/v1/contacts/" + id;
+
+      $http.delete(url)
+        .then(function (response) {
+          getContacts();
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    }
+
     $scope.app = "Lista Telefonica";
 
     $scope.contact = {}
 
-    $scope.contactList = [
-      {
-        name: 'Fulano',
-        number: '9999-9999'
-      },
-      {
-        name: 'Beltrano',
-        number: '8888-9997'
-      }
-    ];
-
     $scope.createContact = function (contact) {
-      $scope.contactList.push(angular.copy(contact));
-      delete $scope.contact;
+      saveContact(contact);
     }
+
+    $scope.deleteContact = function (id) {
+      removeContact(id);
+    }
+
+    getContacts();
   }
 ]);
