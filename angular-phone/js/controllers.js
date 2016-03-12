9ce68eac43f8a03b2cea9ec68740e8da7704addb
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-03-12 14:41:26 -0300

    listing contacts and creating form

--- angular-phone/js/controllers.js
+++ angular-phone/js/controllers.js
@@ -1,6 +1,27 @@
 phoneList
 
 .controller('phoneListCtrl', [
+  '$scope',
 
-  function () {}
+  function ($scope) {
+    $scope.app = "Lista Telefonica";
+
+    $scope.contact = {}
+
+    $scope.contactList = [
+      {
+        name: 'Fulano',
+        number: '9999-9999'
+      },
+      {
+        name: 'Beltrano',
+        number: '8888-9997'
+      }
+    ];
+
+    $scope.createContact = function (contact) {
+      $scope.contactList.push(angular.copy(contact));
+      delete $scope.contact;
+    }
+  }
 ]);
